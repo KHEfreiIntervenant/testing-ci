e1fb6d943eace8f98644fc6dc7e44c8d98bd7e61
Khodor Hammoud <khodor.hammoud@intervenants.efrei.net>
2025-06-11 11:02:59 +0200

    added tests to the list component

--- app/List.tsx
+++ app/List.tsx
@@ -4,9 +4,7 @@ import { useState } from "react";
 
 export default function List() {
     const [list, setList] = useState<string[]>([]);
-    const [input, setInput] = useState<string>("");
     return <div className="flex flex-col gap-2">
-        <input type="text" value={input} onChange={(e) => setInput(e.target.value)} />
         <button id="add" className="bg-blue-500 text-white p-2 rounded-md" onClick={() => setList([...list, "new item"])}>Add</button>
         <ul>
             {list.map((item, index) => (
