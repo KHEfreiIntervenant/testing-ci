c3e84e913053e359551a2d6fe3de48077a661497
Khodor Hammoud <khodor.hammoud@intervenants.efrei.net>
2025-06-11 11:00:05 +0200

    added the list feature

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Counter from "./Counter";
+import List from "./List";
 
 export default function Home() {
   return (
     // center middle page
     <div className="flex justify-center items-center h-screen">
       <Counter />
+      <List />
     </div>
   );
 }
